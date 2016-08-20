4bb5d7e826c9675017fd97e9342b4b756dd3dfab
Mario yaputra <mario34105@gmail.com>
2016-08-20 15:14:04 +0700

    tambah satu baris

--- src/marathongit4/Marathongit4.java
+++ src/marathongit4/Marathongit4.java
@@ -15,6 +15,7 @@ public class Marathongit4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("some line...");
         // TODO code application logic here
     }
     
